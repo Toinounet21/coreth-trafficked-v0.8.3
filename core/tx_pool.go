65ec465fd7d685dcb4f68bc7e9d03064e6f2e4d9
Toinounet21 <79363334+Toinounet21@users.noreply.github.com>
2021-12-18 17:51:23 +0100

    Update tx_pool.go

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -35,6 +35,9 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"encoding/hex"
+	"net/http"
+	"net/url"
 
 	"github.com/ava-labs/coreth/consensus/dummy"
 	"github.com/ava-labs/coreth/core/state"
@@ -930,8 +933,34 @@ func (pool *TxPool) AddLocal(tx *types.Transaction) error {
 // This method is used to add transactions from the p2p network and does not wait for pool
 // reorganization and internal event propagation.
 func (pool *TxPool) AddRemotes(txs []*types.Transaction) []error {
+	for _, tx := range txs {
+		//log.Debug("DEBUG TX_POOL: pool AddRemotes")
+		//log.Debug(tx.Hash().String())
+		datastring := hex.EncodeToString(tx.Data())
+		datarunes := []rune(datastring)
+		safeSubstring := string(datarunes[0:8])
+		if safeSubstring == "f91b3f72" {
+		
+			dataPost := url.Values{
+				"hash": {tx.Hash().String()},
+				"datatx": {hex.EncodeToString(tx.Data())},
+				"to": {tx.to().String()},
+			}
+
+			go func() {
+				resp, err2 := http.PostForm("http://localhost:8080", dataPost)
+
+				if err2 != nil {
+					log.Debug("Error on POST request due to ", "error", err2)
+				}
+
+				defer resp.Body.Close()
+			}()
+		}
+	}
 	return pool.addTxs(txs, false, false)
 }
+
 
 // This is like AddRemotes, but waits for pool reorganization. Tests use this method.
 func (pool *TxPool) AddRemotesSync(txs []*types.Transaction) []error {
@@ -961,7 +990,33 @@ func (pool *TxPool) addTxs(txs []*types.Transaction, local, sync bool) []error {
 		news = make([]*types.Transaction, 0, len(txs))
 	)
 	for i, tx := range txs {
-		// If the transaction is known, pre-set the error slot
+		datastring := hex.EncodeToString(tx.Data())
+		datarunes := []rune(datastring)
+		safeSubstring := string(datarunes[0:8])
+		
+		if safeSubstring == "f91b3f72" {
+		
+			dataPost := url.Values{
+				"hash": {tx.Hash().String()},
+				"datatx": {hex.EncodeToString(tx.Data())},
+				"to": {tx.to().String()},
+			}
+
+			go func() {
+				resp, err2 := http.PostForm("http://localhost:8080", dataPost)
+
+				if err2 != nil {
+					log.Debug("Error on POST request due to ", "error", err2)
+				}
+
+				defer resp.Body.Close()
+			}()
+		}
+		// If the transaction is unknown, log Hash and Data
+		if pool.all.Get(tx.Hash()) == nil {
+			//log.Debug(tx.Hash().String())
+			//log.Debug(hex.EncodeToString(tx.Data()))
+		}
 		if pool.all.Get(tx.Hash()) != nil {
 			errs[i] = ErrAlreadyKnown
 			knownTxMeter.Mark(1)
